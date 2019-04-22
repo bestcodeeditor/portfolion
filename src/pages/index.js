c2b7f1dca3cb13fdc8dc17fe4eb064a1eb082617
sudip <sudipzoom@rediffmail.com>
2019-04-22 16:13:42 +0530

    fifth commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Helmet from "react-helmet";
-// import { graphql } from "gatsby"
-// import Img from "gatsby-image"
 
 import Header from "../components/header";
 import Projects from "../components/project";
